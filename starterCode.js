d539f6b61a8e48eb563a5895c04aa1a6d9a6d592
hamletnscc <113032060+hamletnscc@users.noreply.github.com>
2023-11-01 08:52:40 -0300

    Create starterCode.js

---
+++ starterCode.js
@@ -0,0 +1,42 @@
+"use strict";
+let readlineSync = require('readline-sync');
+
+function getHighestCommonDivisor(in_num1, in_num2)
+{
+  
+}
+
+/*
+Tips: The Number.isInteger() method returns true if a value is an integer of the datatype Number. 
+Otherwise it returns false.
+
+Examples:
+Number.isInteger(-123); //return true
+Number.isInteger('123'); //return false
+Number.isInteger(12.3); //return false
+Number.isInteger("Great!"); //return false
+*/
+function validateNumericInput(in_message)
+{
+ 
+}
+    
+
+function main()
+{
+ 
+}
+
+
+//PROGRAM STARTS HERE
+if (require.main === module)
+{
+    main();
+}
+
+
+//do not change the following lines
+module.exports = {
+   getHighestCommonDivisor,
+   validateNumericInput
+}
